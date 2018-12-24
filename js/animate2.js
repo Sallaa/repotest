36ac64d05faa8e58f528a07ea3fb18f74071125d
Valentin P <valentin.porcellini@gmail.com>
2018-12-25 00:08:22 +0900

    test fix

--- js/animate2.js
+++ js/animate2.js
@@ -6,9 +6,23 @@ $(document).ready(function ($) {
     // });
 
     $("#teste").click(function () {
-        alert("Handler for .click() called.");
-        
+        // alert("Handler for .click() called.");
+        $("#image-6").attr("xlink:href", imgPath[i]).fadeIn("slow");
+        if(imgPath.length == i)
+            i = 0;
+        else
+            i++;
     });
 });
 
-var imgPath = "img/photo1.jpg";
+var i=0;
+var imgPath = ["img/photo2.jpg",
+    "img/photo3.jpg",
+    "img/photo4.jpg",
+    "img/photo5.jpg",
+    "img/photo6.jpg",
+    "img/photo7.jpg",
+    "img/photo8.jpg",
+    "img/photo9.jpg",
+    "img/photo1.jpg",
+];
